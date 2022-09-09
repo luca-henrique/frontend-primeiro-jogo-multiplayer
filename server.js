a3ba7ae6105c9354e005f5805e3c37ae4679657f
Lucas Henrique <lukas.paes18@gmail.com>
2022-09-09 12:59:32 -0300

    feature: adicionando e configurado nodemon

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 import express from 'express';
-
 import http from 'http';
 
-const app = express();
+import createGame from './public/game.js';
 
+const app = express();
 const server = http.createServer(app);
+
+const game = createGame();
+
+game.addPlayer({playerId: 'player1', playerX: 0, playerY: 0});
+game.addFruit({fruitId: 'fruit1', fruitX: 2, fruitY: 2});
+
+console.log(game.state);
 
 app.use(express.static('public'));
 
